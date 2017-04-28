f4e9d074b0340e1ee5776d871bde46f5fcaaabef
Janus Nicon <janusnic@gmail.com>
2017-04-28 16:05:34 +0300

    Added unit 1

---
+++ homework/index.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * Homework
+ *
+ * Create a variable and echo it to the screen using PHP's in-built server.
+ *
+ * The different ways you should echo variables to screen are as follows:
+ * - with single quotes and concatenation
+ * - within double quotes
+ * - within double quotes and using concatenation
+ * - within double quotes and encapsulating the variables
+ * - using single quotes within double quotes
+ * - using double quotes, within single quotes
+ * - escaping double quotes, within a double quote surrounded string
+ * - escaping single quotes, within a single quote surrounded string 
+ **/
+
